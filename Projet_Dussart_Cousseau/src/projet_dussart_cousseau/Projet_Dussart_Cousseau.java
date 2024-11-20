ef1d856819d5d656f746b875165e64e625cd9c6e
Alice <alicedussart34@gmail.com>
2024-11-20 10:59:36 +0100

    projet maj

---
+++ Projet_Dussart_Cousseau/src/projet_dussart_cousseau/Projet_Dussart_Cousseau.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_dussart_cousseau;
+
+/**
+ *
+ * @author alice
+ */
+public class Projet_Dussart_Cousseau {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
