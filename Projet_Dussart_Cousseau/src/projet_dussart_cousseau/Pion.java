ea2f8dda4c68bea3a8df669181af2391874115e8
Dorian <doriancousseau17@gmail.com>
2024-11-27 10:24:26 +0100

    remplissage de la classe puis plus création de la couleur des 6 pions

--- Projet_Dussart_Cousseau/src/projet_dussart_cousseau/Pion.java
+++ Projet_Dussart_Cousseau/src/projet_dussart_cousseau/Pion.java
@@ -10,5 +10,50 @@ package projet_dussart_cousseau;
  * @author doria
  */
 public class Pion {
-    
+    // Attribut pour représenter la couleur du pion
+    private Character couleur;
+
+    /**
+     * Constructeur qui initialise le pion avec une couleur donnée.
+     * @param couleur La couleur du pion (par exemple, 'R' pour rouge, 'B' pour bleu, etc.).
+     */
+    public Pion(Character couleur) {
+        this.couleur = couleur;
+    }
+
+    /**
+     * Accesseur pour obtenir la couleur du pion.
+     * @return La couleur du pion.
+     */
+    public Character getCouleur() {
+        return couleur;
+    }
+
+    /**
+     * Redéfinition de la méthode toString pour afficher la couleur du pion.
+     * @return Une représentation textuelle de la couleur du pion.
+     */
+   @Override 
+    public String toString() {
+        return couleur.toString();
+    }
+
+    // Méthode principale pour tester la classe Pion
+    public static void main(String[] args) {
+        // Création de différents objets Pion
+        Pion pion1 = new Pion('R');
+        Pion pion2 = new Pion('B');
+        Pion pion3 = new Pion('V');
+        Pion poin4 = new Pion('J');
+        Pion poin5 = new Pion('B');
+        Pion poin6 = new Pion('N');
+        
+        System.out.println(getCouleur.pion1());
+
+        
+    }
 }
+
+
+
+
