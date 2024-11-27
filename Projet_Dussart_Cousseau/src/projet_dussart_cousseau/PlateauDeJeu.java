7505d92c6149f50d47413fc1869f92332b2826fb
Dorian <doriancousseau17@gmail.com>
2024-11-27 11:05:36 +0100

    creation des methodes et des attributs de la classe plateaudejeu

--- Projet_Dussart_Cousseau/src/projet_dussart_cousseau/PlateauDeJeu.java
+++ Projet_Dussart_Cousseau/src/projet_dussart_cousseau/PlateauDeJeu.java
@@ -4,10 +4,31 @@
  */
 package projet_dussart_cousseau;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author doria
  */
 public class PlateauDeJeu {
+    Combinaison combinaisonSecrete;
+    ArrayList <Combinaison> tentatives;
+    ArrayList <String> reponses;
+    int nbToursMax;
+
+    public PlateauDeJeu(Combinaison combinaisonSecrete, ArrayList<Combinaison> tentatives, ArrayList<String> reponses, int nbToursMax) {
+        this.combinaisonSecrete = combinaisonSecrete;
+        this.tentatives = tentatives;
+        this.reponses = reponses;
+        this.nbToursMax = nbToursMax;
+    }
+public void proposerCombinaison(Combinaison tentative){
     
+}
+public void estVictoire(){
+    
+}
+public void estDefaite(){
+    
+}
 }
